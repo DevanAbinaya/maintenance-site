c2154cb78deb81b94fd32084e39f7ba01daa47d5
Factiven <dabinaya.fathan@gmail.com>
2023-09-24 11:27:26 +0700

    add links

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/DevanAbinaya/Ani-Moopa",
+        permanent: false,
+        basePath: false,
+      },
+      {
+        source: "/discord",
+        destination: "https://discord.gg/v5fjSdKwr2",
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
